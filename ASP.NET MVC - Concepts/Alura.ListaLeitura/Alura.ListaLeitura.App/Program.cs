80f6d8e227bcd5516e3edd9250841f74d9dc33f2
RenanAbbade <renanabbade1997@gmail.com>
2020-03-18 16:33:46 -0300

    ASP.NET MVC initial

---
+++ ASP.NET MVC - Concepts/Alura.ListaLeitura/Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            //Classe de hospedagem do ASP.NET CORE
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel() //Esse metodo diz qual será a implementação do servidor HTTP que iremos utilizar, sendo está o Kestrel
+                .UseStartup<Startup>()//Classe que inicializa o host
+                .Build();//Classe WebHostBuilder é responsavel por construir um servidor WEB, porém é uma interface, sendo necessário o método .Build() para implementa-la.
+            //O host necessita de configurações para ser executado.
+
+            //Para subir o hospedeiro para o mesmo ficar disponivel para receber as chamadas web.
+            host.Run();
+
+            
+
+            
+
+            ImprimeLista(_repo.ParaLer);
+            ImprimeLista(_repo.Lendo);
+            ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
---
+++ ASP.NET MVC - Concepts/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+
+        public void Configure(IApplicationBuilder app)
+        {//Configurar a sequencia "Chegou requisicao e executa o tal método
+            //Fluxo de request-response
+            //IAapplicationBuilder realiza a configuração do Request Pipeline de requisicao para a aplicação
+            app.Run(LivrosParaLer);
+            //O método Run, precisa de um retorno do tipo RequestDelegate
+            //Um requestDelegate é um metodo que tem como retorno tipos Task
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {//Codigo executado quando chegar uma requisicao, recebe um obj com todas as informações referentes a aquela request
+            //Toda informacao encapsulada em uma request especifica fica encapsulada em objs. do tipo HttpContext
+
+            var _repo = new LivroRepositorioCSV();
+
+            //Response é a property do httpcontext que vem como resposta, e escrevemos nossa lista.
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+
+            
+
+
+        }
+    }
+}
